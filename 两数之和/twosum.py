c4e3558b97b80e5389fdfc3a0115aa643ec84904
码有农777 <18717891157@163.com>
2022-01-27 15:51:24 +0800

    Update twosum.py

--- 两数之和/twosum.py
+++ 两数之和/twosum.py
@@ -1,1 +1,10 @@
-
+nums = [2,7,11,15]
+target = 9
+def twosum():
+  d = {}
+  for i in range(len(nums)):
+      x = target - nums[i]
+      if nums[i] in d:
+          return [d[nums[i]],i]
+      else:
+        d[x] = i
