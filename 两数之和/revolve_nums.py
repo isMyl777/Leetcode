d8b4cccffe2af8c98cea67d42bbf493a5f3adc75
码有农777 <18717891157@163.com>
2022-01-27 16:25:51 +0800

    Create revolve_nums.py
    旋转数组

---
+++ 两数之和/revolve_nums.py
@@ -0,0 +1,9 @@
+#旋转数组
+def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k%n
+        nums[:] = nums[n-k:] + nums[:n-k]
+        print(nums)
