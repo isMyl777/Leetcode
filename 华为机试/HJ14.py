2d8b7fecc4ddd40cfe7e61d66a29bbca2b87ed38
码有农777 <18717891157@163.com>
2022-02-08 14:21:51 +0800

    Create HJ14.py
    字符串排序

---
+++ 华为机试/HJ14.py
@@ -0,0 +1,11 @@
+# 字符串排序
+while True:
+  try:
+    num = int(input())
+    res = []
+    for i in range(num):
+      res.append(input())
+    print('\n'.join(sorted(res))
+   except:
+          break
+      
